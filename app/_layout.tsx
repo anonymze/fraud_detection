30e4e9fbc5aea253a5e1c47416199448927eae87
anonymze <anodevfr@gmail.com>
2024-05-20 16:56:11 +0200

    add: disable auto hide splash

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,29 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+import { useColorScheme } from "react-native";
+
+// Prevent the splash screen from auto-hiding
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  console.log({colorScheme});
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="index" />
